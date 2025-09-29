4998780a07b4b6a2ad78a9772e9c4ad687a68e0c
82481 <82481@RAHUL>
2025-09-30 04:53:26 +0530

    commited done

---
+++ tnsif_java_rahul/src/basisc/java/BasicJava.java
@@ -0,0 +1,10 @@
+package basisc.java;
+
+public class BasicJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ tnsif_java_rahul/src/basisc/java/Basicsjava.java
@@ -0,0 +1,10 @@
+package basisc.java;
+
+public class Basicsjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
